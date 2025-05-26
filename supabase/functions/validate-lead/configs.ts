daa9ab7a55e2af986470f4e2f5f235271a67cdc9
Gilang R Ilhami <119667058+gilangrilhami@users.noreply.github.com>
2025-05-26 12:20:58 +0000

    feat: validate-lead functions configurations
    - Create Supbase client using `createClient` from `@supabase/supabase-js`
      where parameters are taken from environment variables `SUPABASE_URL`
      and `SUPABASE_ANON_KEY`.
    - Load `G_OPENAI_API_KEY` from environment variables and set it as
      `OPENAI_API_KEY`.
    
    Signed-off-by: Gilang R Ilhami <119667058+gilangrilhami@users.noreply.github.com>

---
+++ supabase/functions/validate-lead/configs.ts
@@ -0,0 +1,12 @@
+import { z } from "npm:zod";
+import { createClient } from 'npm:@supabase/supabase-js@2'
+
+const _env = z.object({
+    SUPABASE_URL: z.string().url(),
+    SUPABASE_ANON_KEY: z.string(),
+    G_OPENAI_API_KEY: z.string(),
+});
+export const env = _env.parse(Deno.env.toObject());
+
+export const supabaseClient = createClient(env.SUPABASE_URL, env.SUPABASE_ANON_KEY);
+export const OPENAI_KEY = env.G_OPENAI_API_KEY;
